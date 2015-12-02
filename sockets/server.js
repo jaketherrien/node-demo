0bf1aac2cba28eb3fc96fb594cb6b4b1af4ce231
jaketherrien <jake.therrien13@gmail.com>
2015-12-02 12:17:26 -0800

    changes from class

--- sockets/server.js
+++ sockets/server.js
@@ -3,3 +3,49 @@
     main server script for the socket.io chat demo
 */
 
+var net = require('net');
+
+var server = net.createServer();
+//all clients
+var clients = [];
+
+server.on('connection', function(socket) {
+    var name;
+
+    function broadcast(name, message) {
+        clients.forEach(function(client) {
+            if (client != socket) {
+                client.write('[' + name + ']' + message);
+            }
+        });
+    };
+
+    //captures the new client's info
+    clients.push(socket);
+
+    console.log('new connection');
+    socket.write("Hello! What is your name?\n");
+
+    socket.on('data', function(data) {
+        if (!name) {
+            name = data.toString();
+            socket.write('Hi there ' + name + '\n');
+        }
+        else {
+            broadcast(name, data.toString());
+        }
+
+    });
+
+    socket.on('close', function() {
+        console.log('connection closed');
+    });
+    
+});
+
+
+server.on('listening', function() {
+    console.log('server listening...');
+});
+
+server.listen(3000);
